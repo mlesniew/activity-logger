04e477db44052d36e07683ca2e9e0f5b4f088970
Michał Leśniewski <mlesniew@gmail.com>
2021-02-25 19:22:31 +0100

    Add spotify status

--- activity-logger.sh
+++ activity-logger.sh
@@ -19,11 +19,23 @@ get_active_window() {
     wmctrl -x -l | grep "^${ACTIVE_WINDOW_ID}" | head -n1 | cut -c15- | tr '\t' ' '
 }
 
+spotify_status() {
+    {
+        STATUS=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus'|grep 'string "[^"]*"'|sed 's/.*"\(.*\)"[^"]*$/\1/' | tr '[:upper:]' '[:lower:]')
+        if [ -z "$STATUS" ]
+        then
+            echo "stopped"
+        else
+            echo "$STATUS"
+        fi
+    } 2>/dev/null
+}
+
 {
     while true;
     do
         # time, idle time, teams status, tun0 address, active window
-        printf "$(date -u --iso=seconds | cut -d+ -f1)\t$(xprintidle)\t$(teams_status)\t$(ifdata -pa enp0s25)\t$(ifdata -pa wlo1)\t$(ifdata -pa tun0)\t$(get_active_window)\n"
+        printf "$(date -u --iso=seconds | cut -d+ -f1)\t$(xprintidle)\t$(teams_status)\t$(spotify_status)\t$(ifdata -pa enp0s25)\t$(ifdata -pa wlo1)\t$(ifdata -pa tun0)\t$(get_active_window)\n"
         sleep 10
     done;
 } >> ~/.activity.log
