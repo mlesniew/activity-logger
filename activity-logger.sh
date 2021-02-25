8fb3929a1d4a07f5d21370e65c1cab00d31011b5
Michał Leśniewski <mlesniew@gmail.com>
2021-02-25 18:19:11 +0100

    Use bash only

--- activity-logger.sh
+++ activity-logger.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "$0")"
-
-export PYTHONUNBUFFERED=1
-
-run-one-constantly ./activity-logger.py > ~/.activity.log
+{
+    while true;
+    do
+        echo '========='
+        echo "time: $(date -u --iso=seconds | cut -d+ -f1)"
+        echo "active_window: $(xdotool getwindowfocus)"
+        echo "idle_time: $(xprintidle)"
+        if pactl list source-outputs | tr '\n' '|' | sed -E 's#[|][\t ]+#;#g' | tr '|' '\n' | grep -iF skype | grep -qF 'Corked: no;'
+        then
+            echo "teams_meeting: 1"
+        else
+            echo "teams_meeting: 0"
+        fi
+        echo "tun0_address: $(ifdata -pa tun0)"
+        echo 'windows:'
+        wmctrl -x -l
+        sleep 10
+    done;
+} >> ~/.activity.log
